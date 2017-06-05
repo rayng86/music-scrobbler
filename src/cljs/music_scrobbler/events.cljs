84dffd7be73b427b84bae8979111b5263cb253a5
Raymond Ng <rayng86@gmail.com>
2017-06-05 12:44:43 -0400

    Add events

--- src/cljs/music_scrobbler/events.cljs
+++ src/cljs/music_scrobbler/events.cljs
@@ -1,8 +1,30 @@
 (ns music-scrobbler.events
     (:require [re-frame.core :as re-frame]
-              [music-scrobbler.db :as db]))
+              [clojure.string :as string]
+              [music-scrobbler.db :as db]
+              [ajax.core :as ajax :refer [GET POST OPTIONS]]
+              [day8.re-frame.http-fx]))
+
+(def base "http://ws.audioscrobbler.com/2.0/")
 
 (re-frame/reg-event-db
  :initialize-db
  (fn  [_ _]
    db/default-db))
+
+(re-frame/reg-event-db
+ :set-name
+ (fn [db [_ name]]
+   (assoc db :name name)))
+
+;; For User Authentication
+(re-frame/reg-event-db
+ :set-token
+ (fn [db [_ token]]
+   (assoc db :token token)))
+
+(re-frame/reg-event-db
+ :set-api-sig
+ (fn [db [_ api-sig]]
+   (assoc db :api-sig api-sig)))
+
